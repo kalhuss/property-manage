bb191b734a7bc89d51d1d69b105eb1712bf5d829
Kalim <kkh10@aber.ac.uk>
2023-05-01 08:31:29 +0100

    Changed success and cancel url's

--- src/pages/api/payment.ts
+++ src/pages/api/payment.ts
@@ -41,8 +41,8 @@ export default async function handler(
             },
         ],
         mode: "payment",
-        success_url: `http://localhost:3000/offers/success?id=${contractId}`,
-        cancel_url: `http://localhost:3000/offers/cancel`,
+        success_url: `https://property-manage.vercel.app/offers/success?id=${contractId}`,
+        cancel_url: `https://property-manage.vercel.app/offers/cancel`,
     });
 
     // Return the url to the client
